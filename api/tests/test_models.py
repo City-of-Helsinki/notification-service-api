c84a6131b72d8f2e8f44628a4b7204fa346b84ca
Khin Loc <khin.loc@anders.fi>
2020-07-07 14:53:45 +0300

    Add deliver log and user models

---
+++ api/tests/test_models.py
@@ -0,0 +1,13 @@
+import pytest
+from api.factories import DeliveryLogFactory
+from api.models import DeliveryLog
+
+
+@pytest.fixture(autouse=True)
+def autouse_db(db):
+    pass
+
+
+def test_delivery_log_models():
+    DeliveryLogFactory()
+    assert DeliveryLog.objects.count() == 1
