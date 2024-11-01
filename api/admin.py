d9fe2c1a96ea04970ca9ea295ef4184974ba66ea
Niko Lindroos <niko.j.makela@gmail.com>
2024-11-01 15:44:15 +0200

    feat: message logs admin filter with status
    NS-161.

--- api/admin.py
+++ api/admin.py
@@ -4,9 +4,36 @@ from django.utils.translation import gettext_lazy as _
 from api.models import DeliveryLog
 
 
+class MessageStatusListFilter(admin.SimpleListFilter):
+    DELIVERED = "delivered"
+    FAILED = "failed"
+    UNKNOWN = "unknown"
+    CREATED = "created"
+
+    MESSAGE_STATUS_OPTIONS = [DELIVERED, FAILED, UNKNOWN, CREATED]
+
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _("message status")
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = "status"
+
+    def lookups(self, request, model_admin):
+        return map(lambda o: (o, o.upper()), self.MESSAGE_STATUS_OPTIONS)
+
+    def queryset(self, request, queryset):
+        if self.value() in self.MESSAGE_STATUS_OPTIONS:
+            return queryset.filter(report__icontains=self.value())
+        return queryset
+
+
 class DeliveryLogAdmin(admin.ModelAdmin):
     search_fields = ["report"]
     list_display = ["id", "user", "get_number", "get_status"]
+    list_filter = [
+        MessageStatusListFilter,
+    ]
 
     def get_number(self, obj):
         try:
