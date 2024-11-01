8fd461e7bdb92aba95d5ac4ed06c611a9e060b59
Niko Lindroos <niko.j.makela@gmail.com>
2024-11-01 15:39:42 +0200

    feat: delivery log admin search from report field
    NS-161.

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from api.models import DeliveryLog
 
-admin.site.register(DeliveryLog)
+
+class DeliveryLogAdmin(admin.ModelAdmin):
+    search_fields = ["report"]
+    list_display = ["id", "user", "get_number", "get_status"]
+
+    def get_number(self, obj):
+        try:
+            return ", ".join([number for number in obj.report["messages"]])
+        except (TypeError, KeyError):
+            return ""
+
+    get_number.short_description = _("numbers")
+
+    def get_status(self, obj):
+        try:
+            return ", ".join(
+                [
+                    obj.report["messages"][number]["status"]
+                    for number in obj.report["messages"]
+                ]
+            )
+        except (TypeError, KeyError):
+            return ""
+
+    get_status.short_description = _("status")
+
+
+admin.site.register(DeliveryLog, DeliveryLogAdmin)
