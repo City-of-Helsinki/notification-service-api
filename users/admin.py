db42b898ebd586dd57eee8be9430cbf9162ada17
Niko Lindroos <niko.j.makela@gmail.com>
2024-11-05 16:59:23 +0200

    feat: improve user admin view
    NS-54

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 from users.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = (
+        "username",
+        "id",
+        "uuid",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "date_joined",
+        "last_login",
+    )
+    fieldsets = DjangoUserAdmin.fieldsets + (("UUID", {"fields": ("uuid",)}),)
+    readonly_fields = ("uuid",)
+    list_filter = (
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "groups",
+        "date_joined",
+        "last_login",
+    )
+    search_fields = DjangoUserAdmin.search_fields + ("uuid",)
+    date_hierarchy = "date_joined"
