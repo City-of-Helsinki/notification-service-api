f53ca5b80fbdadb55e917552391134fc9664c140
Niko Lindroos <niko.j.makela@gmail.com>
2024-11-12 17:30:44 +0200

    feat(auditlog): extend DeliveryLog admin view with audit logging
    NS-18.
    
    Use the AuditLogModelAdminMixin to apply the audit logging to Delivery
    logs handled by the DeliveryLogAdmin. The CRUD actions are then
    automatically logged to the audit logging database.

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -2,6 +2,8 @@ import pytest
 
 from api.factories import DeliveryLogFactory
 from api.models import DeliveryLog
+from audit_log.enums import Operation, Status
+from audit_log.models import AuditLogEntry
 
 
 @pytest.fixture(autouse=True)
@@ -12,3 +14,24 @@ def autouse_db(db):
 def test_delivery_log_models():
     DeliveryLogFactory()
     assert DeliveryLog.objects.count() == 1
+
+
+def test_delivery_log_manager_with_audit_log():
+    assert AuditLogEntry.objects.count() == 0
+    delivery_log = DeliveryLogFactory()
+    user = delivery_log.user
+    assert (
+        DeliveryLog.objects.all()
+        .with_audit_log(user=user, operation=Operation.READ.value)
+        .count()
+        == 1
+    )
+    assert AuditLogEntry.objects.count() == 1
+    audit_log = AuditLogEntry.objects.first()
+    assert audit_log.message["audit_event"]["origin"] == "notification_service"
+    assert audit_log.message["audit_event"]["operation"] == Operation.READ.value
+    assert audit_log.message["audit_event"]["status"] == Status.SUCCESS.value
+    assert audit_log.message["audit_event"]["target"]["path"] == DeliveryLog.__name__
+    assert audit_log.message["audit_event"]["target"]["object_ids"] == [
+        str(delivery_log.pk)
+    ]
