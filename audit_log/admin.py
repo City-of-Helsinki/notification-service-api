415792ca0d5c750b9467b849e860a0ab844a296b
Niko Lindroos <niko.j.makela@gmail.com>
2024-11-14 16:03:45 +0200

    feat(auditlog): message columns and filters to admin list view
    NS-18.

--- audit_log/admin.py
+++ audit_log/admin.py
@@ -7,8 +7,9 @@ from django.db import connection
 from django.utils.functional import cached_property
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _
 
-from audit_log.enums import Operation
+from audit_log.enums import Operation, Role
 from audit_log.models import AuditLogEntry
 from audit_log.services import audit_log_service, create_api_commit_message_from_request
 
@@ -60,6 +61,44 @@ class AuditLogModelAdminMixin:
         )
 
 
+class AuditLogMessageOperationListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _("operation")
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = "operation"
+
+    options = [operation.value for operation in Operation]
+
+    def lookups(self, request, model_admin):
+        return map(lambda o: (o, o.upper()), self.options)
+
+    def queryset(self, request, queryset):
+        if self.value() in self.options:
+            return queryset.filter(message__icontains=self.value())
+        return queryset
+
+
+class AuditLogMessageRoleListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _("role")
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = "role"
+
+    options = [role.value for role in Role]
+
+    def lookups(self, request, model_admin):
+        return map(lambda o: (o, o.upper()), self.options)
+
+    def queryset(self, request, queryset):
+        if self.value() in self.options:
+            return queryset.filter(message__icontains=self.value())
+        return queryset
+
+
 class LargeTablePaginator(Paginator):
     """
     Paginator that uses PostgreSQL `reltuples` for queryset size.
@@ -95,12 +134,68 @@ class LargeTablePaginator(Paginator):
 class AuditLogEntryAdmin(admin.ModelAdmin):
     exclude = ("message",)
     readonly_fields = ("id", "created_at", "is_sent", "message_prettified")
-    list_display = ("id", "__str__", "created_at", "is_sent")
-    list_filter = ("created_at", "is_sent")
+    list_display = (
+        "id",
+        "get_audit_event_actor_role",
+        "get_audit_event_operation",
+        "get_audit_event_target_path",
+        "get_audit_event_target_objects_count",
+        "get_audit_event_status",
+        "created_at",
+        "is_sent",
+    )
+    list_filter = (
+        AuditLogMessageOperationListFilter,
+        AuditLogMessageRoleListFilter,
+        "created_at",
+        "is_sent",
+    )
 
     # For increasing listing performance
     show_full_result_count = False
     paginator = LargeTablePaginator
+
+    ordering = ["-created_at"]
+
+    def get_audit_event_actor_role(self, obj):
+        try:
+            return obj.message["audit_event"]["actor"]["role"]
+        except (TypeError, KeyError):
+            return ""
+
+    get_audit_event_actor_role.short_description = _("role")
+
+    def get_audit_event_operation(self, obj):
+        try:
+            return obj.message["audit_event"]["operation"]
+        except (TypeError, KeyError):
+            return ""
+
+    get_audit_event_operation.short_description = _("operation")
+
+    def get_audit_event_status(self, obj):
+        try:
+            return obj.message["audit_event"]["status"]
+        except (TypeError, KeyError):
+            return ""
+
+    get_audit_event_status.short_description = _("status")
+
+    def get_audit_event_target_path(self, obj):
+        try:
+            return obj.message["audit_event"]["target"]["path"]
+        except (TypeError, KeyError):
+            return ""
+
+    get_audit_event_target_path.short_description = _("target path")
+
+    def get_audit_event_target_objects_count(self, obj):
+        try:
+            return len(obj.message["audit_event"]["target"]["object_ids"])
+        except (TypeError, KeyError):
+            return ""
+
+    get_audit_event_target_objects_count.short_description = _("targets")
 
     def has_delete_permission(self, request, obj=None):
         return False
