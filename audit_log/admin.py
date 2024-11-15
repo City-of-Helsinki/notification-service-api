f140128ed8aa0960271d8cf581abfbb1f774b241
Niko Lindroos <niko.j.makela@gmail.com>
2024-11-15 17:57:53 +0200

    fix(auditlog): write readlog only of current admin list page objects
    NS-18.
    
    The `get_queryset` gave instances of all the pages and not just the
    currently shown page. Instead of overriding the get_queryset, it is
    needed to override the `get_changelist_instance`, resolve the currently
    visible list of objects and then write the audit read log for them.

--- audit_log/admin.py
+++ audit_log/admin.py
@@ -17,21 +17,44 @@ logger = logging.getLogger(__name__)
 
 
 class AuditLogModelAdminMixin:
-    def get_queryset(self, request):
-        """Write audit logs of the queryset read."""
-        # NOTE: Fetching a single object's edit page reuses the list view queryset
-        return (
-            super()
-            .get_queryset(request)
-            .with_audit_log_and_request(
+    def get_changelist_instance(self, request):
+        """
+        Override get_changelist_instance of the Django ModelAdmin
+        to get all the objects of the paginator page and write
+        audit log READ operations for them.
+        """
+        from django.contrib.admin.views.main import PAGE_VAR
+
+        changelist = super().get_changelist_instance(request)
+        paginator = changelist.paginator
+        page_number = request.GET.get(PAGE_VAR, 1)
+        page = paginator.get_page(page_number)
+        current_page_queryset = page.object_list
+        current_page_queryset.with_audit_log_and_request(
+            request=request, operation=Operation.READ.value
+        )
+        return changelist
+
+    def get_object(self, request, object_id, from_field=None):
+        """
+        Given an object id, read it from the database.
+        Write audit log of the action.
+        """
+        obj = super().get_object(request, object_id, from_field=from_field)
+        if obj is not None:
+            message = create_api_commit_message_from_request(
                 request=request,
                 operation=Operation.READ.value,
+                object_ids=[str(obj.pk)],
+                _type=obj._meta.model_name,
             )
-        )
+            audit_log_service._commit_to_audit_log(message=message)
+        return obj
 
     def save_model(self, request, obj, form, change):
         """
         Given a model instance save it to the database.
+        Write audit log of the action.
         """
         # TODO: add a change log to the audit log message
         message = create_api_commit_message_from_request(
@@ -46,6 +69,7 @@ class AuditLogModelAdminMixin:
     def delete_model(self, request, obj):
         """
         Given a model instance delete it from the database.
+        Write audit log of the action.
         """
         message = create_api_commit_message_from_request(
             request=request,
@@ -57,7 +81,10 @@ class AuditLogModelAdminMixin:
         super().delete_model(request, obj)
 
     def delete_queryset(self, request, queryset):
-        """Given a queryset, delete it from the database."""
+        """
+        Given a queryset, delete it from the database.
+        Write audit log of the action.
+        """
         super().delete_queryset(
             request,
             queryset.with_audit_log_and_request(
